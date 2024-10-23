77620eb2e75311c4703d5649acdc5f7dcf1519a0
Henru du Toit <henru@brightedge.co.za>
2024-10-23 21:05:16 +0200

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from '../hooks/useGames';
 import { Card, CardBody, Heading, Image, Text, HStack } from '@chakra-ui/react';
 import PlatFormIconList from './PlatFormIconList';
 import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
 
 interface GameCardProps {
   game: Game;
@@ -13,7 +14,7 @@ const GameCard = ({ game }: GameCardProps) => {
   // The heading fontSize is defined in chakra
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={'2xl'}>{game.name}</Heading>
         <HStack justifyContent={'space-between'}>
