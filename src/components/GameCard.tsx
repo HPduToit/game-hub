0bbc2bd5123e90a31ed401a65ea2003d7103343b
Henru du Toit <henru@brightedge.co.za>
2024-10-23 21:22:47 +0200

    Created GameCardSkeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,7 +13,7 @@ const GameCard = ({ game }: GameCardProps) => {
   // Therefore we need the overflow = hidden
   // The heading fontSize is defined in chakra
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width='300px' borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={'2xl'}>{game.name}</Heading>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,7 +13,7 @@ export interface Game {
   name: string;
   background_image: string;
   metacritic: number;
-  parent_platforms: {platform: Platform}[]
+  parent_platforms: { platform: Platform }[];
   // The parent_platform is not a Playform[] (platform array)
   // It is a array of objects where each object has a property called platform of type Platform
 }
@@ -26,23 +26,32 @@ interface FetechGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
+  // The correct way to setLoading(false) would be inside the .final method,
+  // but this does not work with the strict mode turned on, so we do it inside
+  // the callback
   useEffect(() => {
     const controller = new AbortController();
-
+    
+    setLoading(true);
     // for the instance of CanceltedError bit refer to 10 - creating a custom Hook for Fetching Games 04:30
     apiClient
       .get<FetechGamesResponse>('/games', { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
