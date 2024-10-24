eef498743b632299354f9bb6cd0394c4ab5b79d7
Henru du Toit <henru@brightedge.co.za>
2024-10-24 21:55:39 +0200

    Show a spinner while fetching genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -5,6 +5,8 @@ interface Props {
   children: ReactNode;
 }
 
+/* For a good explination about the width that was 300px and then changed
+  to 100% (or the default) listen to 19 - Displaying the Genres 07:40*/
 const GameCardContainer = ({ children }: Props) => {
   return (
     <Box width="100%" borderRadius={10} overflow="hidden">
