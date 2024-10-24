986b4e261892071e4213f121554a2a7a0ee0b0d5
Henru du Toit <henru@brightedge.co.za>
2024-10-24 21:50:06 +0200

    Display generes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import "./App.css";
-import { Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import Genrelist from "./components/GenreList";
+import './App.css';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
+import GameGrid from './components/GameGrid';
+import Genrelist from './components/GenreList';
 
 function App() {
   // set the templateAreas to define the layout of the grid
@@ -12,17 +12,22 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      // 1fr means one fraction; so our column stretches and takes up all the available space
+      templateColumns={{
+        base: '1fr',
+        lg: '200px 1fr',
+      }}
     >
       <GridItem area="nav">
-        <NavBar/>
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside">
-          <Genrelist/>
+          <Genrelist />
         </GridItem>
       </Show>
-      <GridItem area="main">
-        <GameGrid/>
+      <GridItem area="main" paddingX={5}>
+        <GameGrid />
       </GridItem>
     </Grid>
   );
