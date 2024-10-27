651291d423d7f43584716476bd24925654488550
Henru du Toit <henru@brightedge.co.za>
2024-10-27 13:21:21 +0100

    Customize Theme to get darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,27 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-    initialColorMode: 'dark'
+  initialColorMode: 'dark',
 };
 
-const theme = extendTheme({config});
+/* To style the theme refer to the Chakra webside 'default theme'. They
+show you how you can implement your own color schemes */
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: '#f9f9f9',
+      100: '#ededed',
+      200: '#d3d3d3c',
+      300: '#b3b3b3',
+      400: '#a0a0a0',
+      500: '#898989',
+      600: '#636363',
+      700: '#202020',
+      800: '#121212',
+      900: '#111',
+    },
+  },
+});
 
-export default theme; 
+export default theme;
